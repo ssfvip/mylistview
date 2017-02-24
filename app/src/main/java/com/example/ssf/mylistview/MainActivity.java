e7c14e38555a0d6dd1dfcc3a6b6c7e324c97f126
H-Destiny <2026652784@qq.com>
2017-02-24 10:59:38 +0800

    first

---
+++ app/src/main/java/com/example/ssf/mylistview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ssf.mylistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 之定义listvie实现视差效果
+ * 既是下拉使得头部的view自带拉伸效果
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
